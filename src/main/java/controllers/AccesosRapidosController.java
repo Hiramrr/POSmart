05787ce9ae2c661eeaf2d6460f05cd2ed3ae8d53
hiramrr <sasuke_77.23@hotmail.com>
2025-05-06 19:35:38 -0600

    Se arreglo un bug

--- src/main/java/controllers/AccesosRapidosController.java
+++ src/main/java/controllers/AccesosRapidosController.java
@@ -27,7 +27,51 @@ public class AccesosRapidosController {
         }
     }
 
+    @FXML
+    private void handleButtonEmpleado(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/agregarEmpleados.fxml"));
+            Parent root = loader.load();
+            rootPane.getChildren().clear();
+            rootPane.getChildren().add(root);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    @FXML
+    private void handleAgregarProductos(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/AgregarProductos.fxml"));
+            Parent root = loader.load();
+            rootPane.getChildren().clear();
+            rootPane.getChildren().add(root);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    @FXML
+    private void handleButtonUbicaciones(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/agregarUbicacion.fxml"));
+            Parent root = loader.load();
+            rootPane.getChildren().clear();
+            rootPane.getChildren().add(root);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    @FXML
+    private void handleButtonCategoria(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/agregarCategoria.fxml"));
+            Parent root = loader.load();
+            rootPane.getChildren().clear();
+            rootPane.getChildren().add(root);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
