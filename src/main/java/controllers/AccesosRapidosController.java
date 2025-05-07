b62f26a3b61f84e7722f3673165e226b36492bfe
SarahBaez <zS23014136@estudiantes.uv.mx>
2025-05-07 07:49:08 -0600

    pantalla Empleado

--- src/main/java/controllers/AccesosRapidosController.java
+++ src/main/java/controllers/AccesosRapidosController.java
@@ -30,7 +30,7 @@ public class AccesosRapidosController {
     @FXML
     private void handleButtonEmpleado(ActionEvent event) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/agregarEmpleados.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/EditarEmpleado.fxml"));
             Parent root = loader.load();
             rootPane.getChildren().clear();
             rootPane.getChildren().add(root);
