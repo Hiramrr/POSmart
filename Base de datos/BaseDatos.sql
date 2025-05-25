bcdcc81b3600496dbf90392b768a69bcf5ce1a5d
hiramrr <sasuke_77.23@hotmail.com>
2025-05-25 17:10:58 -0600

    Se me olvido subir la base de datos jaja

--- Base de datos/BaseDatos.sql
+++ Base de datos/BaseDatos.sql
@@ -1,19 +1,29 @@
--- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.41, for macos15 (arm64)
 --
--- Host: localhost    Database: posmart
+-- Host: localhost    Database: PoSmart
 -- ------------------------------------------------------
--- Server version	8.0.42
+-- Server version	9.2.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `PoSmart`
+--
+
+/*!40000 DROP DATABASE IF EXISTS `PoSmart`*/;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `PoSmart` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `PoSmart`;
 
 --
 -- Table structure for table `categoria`
@@ -58,7 +68,7 @@ CREATE TABLE `compra` (
   KEY `id_Usuario` (`id_Usuario`),
   CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`id_Proveedor`) REFERENCES `proveedor` (`id_Proveedor`),
   CONSTRAINT `compra_ibfk_2` FOREIGN KEY (`id_Usuario`) REFERENCES `usuario` (`id_Usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +77,7 @@ CREATE TABLE `compra` (
 
 LOCK TABLES `compra` WRITE;
 /*!40000 ALTER TABLE `compra` DISABLE KEYS */;
-INSERT INTO `compra` VALUES (1,'2025-05-13',18,1,2),(2,'2025-05-13',18,1,2),(3,'2025-05-13',18,1,2),(4,'2025-05-13',18,1,2),(5,'2025-05-13',8,1,2),(6,'2025-05-13',88,1,2),(7,'2025-05-13',8,1,2);
+INSERT INTO `compra` VALUES (1,'2025-05-13',18,1,2),(2,'2025-05-13',18,1,2),(3,'2025-05-13',18,1,2),(4,'2025-05-13',18,1,2),(5,'2025-05-13',8,1,2),(6,'2025-05-13',88,1,2),(7,'2025-05-13',8,1,2),(8,'2025-05-14',98,1,2);
 /*!40000 ALTER TABLE `compra` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -89,7 +99,7 @@ CREATE TABLE `detalle_compra` (
   KEY `id_Compra` (`id_Compra`),
   CONSTRAINT `detalle_compra_ibfk_1` FOREIGN KEY (`id_Producto`) REFERENCES `productos` (`id_Producto`),
   CONSTRAINT `detalle_compra_ibfk_2` FOREIGN KEY (`id_Compra`) REFERENCES `compra` (`id_compra`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -98,7 +108,7 @@ CREATE TABLE `detalle_compra` (
 
 LOCK TABLES `detalle_compra` WRITE;
 /*!40000 ALTER TABLE `detalle_compra` DISABLE KEYS */;
-INSERT INTO `detalle_compra` VALUES (1,3,4,1,18),(2,1,5,1,8),(3,1,6,1,8),(4,4,6,1,35),(5,2,6,1,25),(6,5,6,1,20),(7,1,7,1,8);
+INSERT INTO `detalle_compra` VALUES (1,3,4,1,18),(2,1,5,1,8),(3,1,6,1,8),(4,4,6,1,35),(5,2,6,1,25),(6,5,6,1,20),(7,1,7,1,8),(8,2,8,1,25),(9,3,8,1,18),(10,5,8,1,20),(11,4,8,1,35);
 /*!40000 ALTER TABLE `detalle_compra` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -158,7 +168,7 @@ CREATE TABLE `productos` (
 
 LOCK TABLES `productos` WRITE;
 /*!40000 ALTER TABLE `productos` DISABLE KEYS */;
-INSERT INTO `productos` VALUES (1,'Manzana Roja','Fruta fresca y crujiente',102,5,8,1,3),(2,'Jabón Líquido','Jabón antibacterial para manos',77,15,25,2,2),(3,'Pasta de Dientes','Con flúor y protección anticaries',96,10,18,2,2),(4,'Queso Fresco','Producto lácteo refrigerado',59,20,35,1,3),(5,'Cuaderno Universitario','100 hojas rayadas, tapa dura',49,12,20,3,1);
+INSERT INTO `productos` VALUES (1,'Manzana Roja','Fruta fresca y crujiente',103,5,8,1,3),(2,'Jabón Líquido','Jabón antibacterial para manos',154,15,25,2,2),(3,'Pasta de Dientes','Con flúor y protección anticaries',95,10,18,2,2),(4,'Queso Fresco','Producto lácteo refrigerado',68,20,35,1,3),(5,'Cuaderno Universitario','100 hojas rayadas, tapa dura',53,12,20,3,1);
 /*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -245,11 +255,30 @@ INSERT INTO `usuario` VALUES (1,'Miyu','123','Miyu Maldonado','Administrador','2
 UNLOCK TABLES;
 
 --
--- Dumping events for database 'posmart'
+-- Temporary view structure for view `vista_productos`
+--
+
+DROP TABLE IF EXISTS `vista_productos`;
+/*!50001 DROP VIEW IF EXISTS `vista_productos`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `vista_productos` AS SELECT 
+ 1 AS `id_Producto`,
+ 1 AS `Nombre`,
+ 1 AS `Descripcion`,
+ 1 AS `Cantidad_stock`,
+ 1 AS `Precio_compra`,
+ 1 AS `Precio_venta`,
+ 1 AS `categoria`,
+ 1 AS `ubicacion`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping events for database 'PoSmart'
 --
 
 --
--- Dumping routines for database 'posmart'
+-- Dumping routines for database 'PoSmart'
 --
 /*!50003 DROP PROCEDURE IF EXISTS `agregar_Categoria` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
@@ -329,6 +358,68 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `agregar_producto` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`Hiram`@`%` PROCEDURE `agregar_producto`(
+    IN p_id_producto INT,
+    IN p_nombre VARCHAR(100),
+    IN p_descripcion VARCHAR(200),
+    IN p_cantidad_stock INT,
+    IN p_precio_compra INT,
+    IN p_precio_venta INT,
+    IN p_nombre_categoria VARCHAR(50),
+    IN p_nombre_ubicacion VARCHAR(50)
+)
+BEGIN
+    DECLARE v_id_categoria INT;
+    DECLARE v_id_ubicacion INT;
+
+    -- Buscar la categoría
+    SELECT id_Categoria INTO v_id_categoria
+    FROM Categoria
+    WHERE Nombre = p_nombre_categoria
+    LIMIT 1;
+
+    -- Si no existe, insertarla
+    IF v_id_categoria IS NULL THEN
+        INSERT INTO Categoria(Nombre, Descripcion) VALUES (p_nombre_categoria, 'Categoría agregada automáticamente');
+        SET v_id_categoria = LAST_INSERT_ID();
+    END IF;
+
+    -- Buscar la ubicación
+    SELECT id_Ubicacion INTO v_id_ubicacion
+    FROM Ubicacion
+    WHERE Nombre = p_nombre_ubicacion
+    LIMIT 1;
+
+    -- Si no existe, insertarla
+    IF v_id_ubicacion IS NULL THEN
+        INSERT INTO Ubicacion(Nombre, Descripcion) VALUES (p_nombre_ubicacion, 'Ubicación agregada automáticamente');
+        SET v_id_ubicacion = LAST_INSERT_ID();
+    END IF;
+
+    -- Insertar el producto
+    INSERT INTO Productos (
+        id_Producto, Nombre, Descripcion, Cantidad_stock,
+        Precio_compra, Precio_venta, id_Categoria, id_Ubicacion
+    ) VALUES (
+                 p_id_producto, p_nombre, p_descripcion, p_cantidad_stock,
+                 p_precio_compra, p_precio_venta, v_id_categoria, v_id_ubicacion
+             );
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `agregar_proveedor` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -383,56 +474,6 @@ BEGIN
     SELECT id_Ubicacion AS Ubicacion_agregada
     FROM Ubicacion
     WHERE id_Ubicacion = p_id;
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `agregar_usuario` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`Hiram`@`%` PROCEDURE `agregar_usuario`(p_id int, p_Nombre_usuario VARCHAR(20), p_Contraseña VARCHAR(50), p_Nombre_completo VARCHAR(50), p_Rol VARCHAR(20), p_Telefono VARCHAR(15), p_Ciudad VARCHAR(30), p_Direccion VARCHAR(100)
-)
-BEGIN
-    INSERT INTO Usuario (id_usuario, Nombre_usuario, Contraseña, Nombre_completo, Rol, Telefono, Ciudad, Direccion)
-    VALUES (p_id, p_Nombre_usuario, p_Contraseña, p_Nombre_completo, p_Rol, p_Telefono, p_Ciudad, p_Direccion);
-    
-    SELECT id_usuario
-    FROM Usuario
-    WHERE id_usuario = p_id;
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `cambiar_contraseña` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`Hiram`@`%` PROCEDURE `cambiar_contraseña`(Nombre_Com varchar(100), Nueva_contraseña varchar(255))
-BEGIN
-UPDATE Usuario
-SET Contraseña = Nueva_contraseña
-WHERE Nombre_completo = Nombre_com;
-
-SELECT id_Usuario
-FROM Usuario
-WHERE Contraseña = Nueva_contraseña;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -646,6 +687,30 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Current Database: `PoSmart`
+--
+
+USE `PoSmart`;
+
+--
+-- Final view structure for view `vista_productos`
+--
+
+/*!50001 DROP VIEW IF EXISTS `vista_productos`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`Hiram`@`%` SQL SECURITY DEFINER */
+/*!50001 VIEW `vista_productos` AS select `p`.`id_Producto` AS `id_Producto`,`p`.`Nombre` AS `Nombre`,`p`.`Descripcion` AS `Descripcion`,`p`.`Cantidad_stock` AS `Cantidad_stock`,`p`.`Precio_compra` AS `Precio_compra`,`p`.`Precio_venta` AS `Precio_venta`,`c`.`Nombre` AS `categoria`,`u`.`Nombre` AS `ubicacion` from ((`productos` `p` join `categoria` `c` on((`p`.`id_categoria` = `c`.`id_Categoria`))) join `ubicacion` `u` on((`p`.`id_ubicacion` = `u`.`id_Ubicacion`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -656,4 +721,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-13 17:15:37
+-- Dump completed on 2025-05-25 17:08:50
